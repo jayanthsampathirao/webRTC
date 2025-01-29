623859251798a82ef207a5e134e3c1380374c2b9
S Jayanth <sjayanth@Ss-MacBook-Air.local>
2025-01-30 02:19:38 +0530

    Added server directory

--- server/index.js
+++ server/index.js
@@ -26,9 +26,10 @@ io.on("connection",(socket)=>{
     socket.on( 'peer:nego:needed',({to,offer})=>{
         io.to(to).emit('peer:nego:needed',{from : socket.id , offer});
     })
-    socket.on( 'peer:nego:done',({to,offer})=>{
-        io.to(to).emit('peer:nego:done',{from : socket.id , offer});
+    socket.on( 'peer:nego:done',({to,ans})=>{
+        io.to(to).emit('peer:nego:final ',{from : socket.id , ans});
     })
+    
 
     
 
